5acb793d18cfc495948d006e2b1d08466775f015
Marvin Diaz <marvindaviddiaz@gmail.com>
2018-02-21 20:58:16 -0600

    Benchmark

--- theysaidso/theysaidso_test.go
+++ theysaidso/theysaidso_test.go
@@ -1,11 +1,19 @@
 package theysaidso
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestTimeConsuming(t *testing.T) {
+func TestPrintQuoteOfTheDay(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping test in short mode.")
 	}
 
 	PrintQuoteOfTheDay()
+}
+
+func BenchmarkPrintQuoteOfTheDay(b *testing.B) {
+	for i := 0; i <b.N; i++ {
+		PrintQuoteOfTheDay()
+	}
 }
